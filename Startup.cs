ec1cdcf2a9e75ed64a1353926f5b0d832da97271
Andrew Volkov <derbylive145@gmail.com>
2019-12-20 19:59:24 +0300

    add twitter

--- Startup.cs
+++ Startup.cs
@@ -23,12 +23,10 @@ namespace QandAn
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -48,13 +46,19 @@ namespace QandAn
                 facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];  
             });
             
+            services.AddAuthentication().AddTwitter(twitterOptions => {  
+                twitterOptions.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];  
+                twitterOptions.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];  
+            });  
+
+            
             services.AddTransient<StackService>();
             services.AddSingleton<IEmailSender, EmailSender>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
