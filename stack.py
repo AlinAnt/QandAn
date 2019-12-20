af78b5bfb7dbe3cf0bc98ba879d8aa8d35b86723
Alina_Antoniuk <antoniuk642@gmail.com>
2019-12-20 05:08:30 +0300

    fixed html tags

--- stack.py
+++ stack.py
@@ -7,15 +7,21 @@ def get_question(url):
     res = requests.get(f'https://api.stackexchange.com/2.2/questions/{q_id}', {'site': 'stackoverflow', 'filter': 'withbody', 'answers':'True'})
     return res.json()['items'][0]
 
+def data_cleaner(text):
+    pattern_1 = re.compile( r'(<[^>]+>)|((www\.[^ ]+)\b)|((https?://)\S+)', re.I)
+    lower_case = text.lower()
+
+    return re.sub(pattern_1, '', lower_case)
+  
 
 if __name__ == "__main__":
     url =  sys.argv[1]
     question = get_question(url)
 
     with open('title.txt', 'w', encoding='utf-8') as g:
-        g.write(str(question['title']))
+        g.write(data_cleaner(str(question['title'])))
     
     with open('body.txt', 'w', encoding='utf-8') as g:
-        g.write(str(question['body']))
+        g.write(data_cleaner(str(question['body'])))
 
 
